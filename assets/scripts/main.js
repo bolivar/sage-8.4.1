231fbfb7440eaba57b7ed273dcaabe41406310fb
bolivar <manuel.b.lema@gmail.com>
2016-02-22 22:43:41 -0500

    slider is working
    slider is working plus scroll up header is also working

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -71,10 +71,46 @@
           }
 
           //fixed menu js
-          $(document).ready(function() {
-            $('.banner').scrollToFixed();
+          // Hide Header on on scroll down
+          var didScroll;
+          var lastScrollTop = 0;
+          var delta = 5;
+          var navbarHeight = $('.banner').outerHeight();
+
+          $(window).scroll(function(event){
+              didScroll = true;
           });
 
+          setInterval(function() {
+              if (didScroll) {
+                  hasScrolled();
+                  didScroll = false;
+              }
+          }, 250);
+
+          function hasScrolled() {
+              var st = $(this).scrollTop();
+
+              // Make sure they scroll more than delta
+              if(Math.abs(lastScrollTop - st) <= delta)
+                  return;
+
+              // If they scrolled down and are past the navbar, add class .nav-up.
+              // This is necessary so you never see what is "behind" the navbar.
+              if (st > lastScrollTop && st > navbarHeight){
+                  // Scroll Down
+                  $('.banner').removeClass('nav-down').addClass('nav-up');
+              } else {
+                  // Scroll Up
+                  if(st + $(window).height() < $(document).height()) {
+                      $('.banner').removeClass('nav-up').addClass('nav-down');
+                  }
+              }
+
+              lastScrollTop = st;
+          }
+
+          // end fixed menu js
 
           // slick carousel js
           $('.center').slick({
